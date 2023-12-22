1c863ff01e1a47cbbcb5f610b8ae266eda2ad6dc
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-12-22 15:37:22 +0200

    new prompt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import React, {useState} from 'react';
 
+import serious from './serious.png';
+import funny from './funny.png';
+import funny_disabled from './funny-disabled.png';
+import serious_disabled from './serious-disabled.png';
+
 function App() {
   const [theory, setTheory] = useState("");
   const [fact, setFact] = useState("");
+  const [mode, setMode] = useState(true); // true is funny
 
   const onChangeHandler = event => {
     setTheory(event.target.value);
   };
+
+  const seperator = "TLDR";
 
   function handle(e){
     if(!e.key || e.key === "Enter"){
@@ -17,8 +25,8 @@ function App() {
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
-                document.querySelectorAll(".lds-roller")[0].style.setProperty('display', 'none');
-                setFact(this.response);
+              document.querySelectorAll(".lds-roller")[0].style.setProperty('display', 'none');
+              setFact(this.response);
             }  
         };
         xhr.open("POST", 'https://rt09w8q66h.execute-api.us-east-1.amazonaws.com/', true);
@@ -27,7 +35,10 @@ function App() {
         xhr.setRequestHeader('X-Referer', window.location.href);
         xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
         xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(theory);
+        xhr.send(JSON.stringify({
+          theory: theory,
+          funny_mode: mode
+        }));
     }
   }
 
@@ -36,6 +47,10 @@ function App() {
       <h1 className="title">Fact Checker</h1>
       <input placeholder="Paste text to be reviewed here and click “really?”" className="theory" onChange={onChangeHandler} onKeyPress={handle} value={theory}></input>
       <div className="button" onClick={handle}> Really? </div>
+      <div className="responseStyle"> 
+        <img src={mode ? serious_disabled : serious} onClick={(e)=>{setMode(false);handle(e)}} alt="serious mode" />
+        <img src={mode ? funny : funny_disabled} onClick={(e)=>{setMode(true);handle(e)}} alt="funny mode" />
+      </div>
       <div className="fact">{fact}</div>
 
       <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
