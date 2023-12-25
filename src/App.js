e31228d2c9b736ba0edac6b40667e28ddccb1ba0
Yotam Kadishay <yotamk@YotamK-BM.local>
2023-12-25 16:18:07 +0200

    share basics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import React, { useState } from 'react';
 //import funny_disabled from './funny-disabled.png';
 //import serious_disabled from './serious-disabled.png';
 
+import twitterX from './twitter-x-white.png';
+import copyClip from './copy-clip-white.png';
+
 function App() {
   const [theory, setTheory] = useState("");
   const [fact, setFact] = useState("");
@@ -15,7 +18,7 @@ function App() {
     setTheory(event.target.value);
   };
 
-  function handle(e,){
+  function handle(e){
     if(!e.key || e.key === "Enter"){
       setFact("");
       document.querySelectorAll(".lds-roller")[0].style.setProperty('display', 'block');
@@ -40,6 +43,14 @@ function App() {
     }
   }
 
+  function copyText(onlyX) {
+    let textToCopy = fact.fact + "\n https://www.vereally.com/" ;
+    if (onlyX) {
+      textToCopy = fact.fact.split("X - ")[1].split("SUMMARY")[0] + " https://www.vereally.com/";
+    }
+    navigator.clipboard.writeText(textToCopy);
+  }
+
   return (
     <div className="App">
       <h1 className="title">Fact Checker</h1>
@@ -56,18 +67,30 @@ function App() {
         }
       */}
       
-      <div className="fact">
-        {fact ? fact.fact : ""}
-        <br />
-        <div className="fact-sources-title">{fact && fact.sources ? "Sources:" : ""}</div>
-        <div className="fact-sources">{fact && fact.sources ? fact.sources.map((source)=> <div className="fact-card">
-          <div className="fact-card-title">{source.title}</div>
-          <div className="fact-card-content">{source.content}</div>
-          <a href={source.url} className="fact-card-url">{source.url}</a>
-        </div>) : ""}</div>
+      <div className="fact-container">
+        {fact ? <div id="fact" className="fact"> {fact.fact} </div> : ""}
+        
+        {fact ? 
+          <div>
+            <br />
+            <img className="copy-button" src={twitterX} onClick={()=>{copyText(true)}} />
+            <img className="copy-button" src={copyClip} onClick={()=>{copyText(false)}} />
+          </div>
+        : ""}
+
+        {fact && fact.sources ? <div>
+          <br />
+          <div className="fact-sources-title">{fact && fact.sources ? "Sources:" : ""}</div>
+          <div className="fact-sources">{fact && fact.sources ? fact.sources.map((source)=> <div className="fact-card">
+              <div className="fact-card-title">{source.title}</div>
+              <div className="fact-card-content">{source.content}</div>
+              <a href={source.url} className="fact-card-url">{source.url}</a>
+            </div>) : ""}
+          </div>
+        </div> : ""}
+
       </div>
       
-
       <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
   );
